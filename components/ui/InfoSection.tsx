7312cfa2921f179c9b3387dbecb7b7449469e2cb
@yuri_assuncx <killerkoll2012@gmail.com>
2023-08-15 23:48:47 -0300

    feat: responsive layout to Info Section & Breadcrumb

--- components/ui/InfoSection.tsx
+++ components/ui/InfoSection.tsx
@@ -20,8 +20,8 @@ export interface Props {
 function Info({ firstContent, label }: InfoProps) {
   return (
     <div class="flex flex-col text-center text-darkgray">
-      <h1 class="text-[40px]">{firstContent}</h1>
-      <span>{label}</span>
+      <h1 class="text-3xl md:text-[40px]">{firstContent}</h1>
+      <span class="text-sm md:text-base">{label}</span>
     </div>
   );
 }
@@ -30,18 +30,24 @@ export default function InfoSection(
   { title, subtitle, details, image, infos }: Props,
 ) {
   return (
-    <section class="flex w-full h-full lg:min-h-[820px] py-12 px-2 lg:px-0">
-      <div class="flex w-full items-center justify-center">
-        <div class="lg:max-w-4xl w-full flex items-center justify-between gap-6 md:gap-0 px-20 lg:px-0 relative">
+    <section class="flex w-full h-full lg:min-h-[980px] py-12 px-2 lg:px-0">
+      <div class="flex flex-col w-full items-center justify-center">
+        <div class="lg:max-w-4xl w-full flex items-center justify-between gap-6 md:gap-0 px-4 md:px-20 lg:px-0 relative">
           <div class="hidden md:block absolute inset-0 bg-mask-gradient-2 -translate-x-[62%] lg:-translate-x-[72%] -translate-y-44 lg:-translate-y-52 bg-no-repeat bg-cover w-[788px] h-[738px]" />
-          <div class="flex flex-col gap-12">
+          <div class="hidden md:flex flex-col gap-12">
             {infos?.map((info) => <Info {...info} />)}
           </div>
 
-          <div class="flex flex-col sm:max-w-full md:max-w-[65%] lg:max-w-[547px]">
-            <h1 class="text-white text-[40px]">{title}</h1>
-            <span class="text-pink text-2xl pt-[30px]">{subtitle}</span>
-            <p class="text-darkgray pt-3">{details}</p>
+          <div class="flex flex-col items-center justify-center md:items-stretch md:justify-stretch text-center md:text-start max-w-full md:max-w-[65%] lg:max-w-[547px]">
+            <h1 class="text-white text-2xl md:text-[32px] text-[40px]">
+              {title}
+            </h1>
+            <span class="text-pink text-xl md:text-2xl pt-[30px]">
+              {subtitle}
+            </span>
+            <p class="text-darkgray pt-3 text-sm md:text-base lg:text-lg">
+              {details}
+            </p>
             {image && (
               <Image
                 class="pt-[30px] max-h-full"
@@ -52,11 +58,16 @@ export default function InfoSection(
                 loading="lazy"
               />
             )}
-            <button class="flex items-center justify-center rounded-2xl lg:rounded-xl bg-pink py-3 max-w-[142px] h-[48px] border text-black font-semibold border-pink hover:text-pink hover:bg-midnightblue transition-all duration-150 mt-11">
+            <div class="flex md:hidden gap-6">
+              {infos?.map((info) => <Info {...info} />)}
+            </div>
+            <button class="flex items-center justify-center rounded-2xl lg:rounded-xl bg-pink py-3 min-w-[142px] md:max-w-[142px] h-[48px] border text-black font-semibold border-pink hover:text-pink hover:bg-midnightblue transition-all duration-150 mt-11">
               Buy now
             </button>
           </div>
         </div>
+
+        <div class="bg-line-gradient w-[90%] h-1 mt-32" loading="lazy" />
       </div>
     </section>
   );
