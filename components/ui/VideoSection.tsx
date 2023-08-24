8344d6c95035f9a65d1fa53a076c6b6433ab6276
NetinhoSZ <netinhoskennedy@gmail.com>
2023-08-24 17:44:06 -0300

    Fix: VideoSection Gradient Correction

--- components/ui/VideoSection.tsx
+++ components/ui/VideoSection.tsx
@@ -30,7 +30,7 @@ export default function ({ title, subTitle, video }: Props) {
 
   return (
     <>
-      <div class="h-full w-full bg-gradient-to-b from-[#141e2d] to-midnightblue overflow-hidden">
+      <div class="h-full w-full pb-28 bg-gradient-to-b from-[#141e2d] to-midnightblue overflow-hidden">
         <div class="flex items-center justify-center text-center relative w-full animate-slide-bottom">
           <img
             loading="lazy"
@@ -38,7 +38,7 @@ export default function ({ title, subTitle, video }: Props) {
             width={771}
             height={738}
             src={asset("/gradient-vsection-1.webp")}
-            class="hidden md:translate-x-[-39.5%] md:translate-y-[74%] lg:translate-x-[-39.5%] lg:translate-y-[74%] lg:block absolute video-section-gradient-2xl inset-0 bg-no-repeat bg-cover w-[644px] h-[617px]"
+            class="hidden md:translate-x-[-45%] md:translate-y-[62%] md:rotate-180 lg:translate-x-[-50%] lg:translate-y-[90%] lg:rotate-0 xl:translate-x-[-40%] xl:translate-y-[90%] 2xl:translate-x-[-20%] 2xl:translate-y-[90%] 3xl:translate-x-[-12%] 3xl:translate-y-[90%] md:block absolute inset-0 bg-no-repeat bg-cover w-[644px] h-[617px]"
           />
 
           <img
@@ -47,7 +47,7 @@ export default function ({ title, subTitle, video }: Props) {
             width={771}
             height={738}
             src={asset("/gradient-vsection-2.webp")}
-            class="hidden lg:translate-x-[138%] lg:translate-y-[72%] md:translate-x-[200%] md:translate-y-[72%]  lg:block absolute video-section-gradient-2-2xl inset-0 bg-no-repeat bg-cover w-[644px] h-[617px]"
+            class="hidden md:translate-x-[65%] md:translate-y-[62.9%] md:rotate-180 lg:translate-x-[108%] lg:translate-y-[87.8%] lg:rotate-0 xl:translate-x-[139%] xl:translate-y-[87.8%] 2xl:translate-x-[158%] 2xl:translate-y-[87.8%] 3xl:translate-x-[207%] 3xl:translate-y-[87.8%] md:block absolute inset-0 bg-no-repeat bg-cover w-[644px] h-[617px]"
           />
 
           <div class="min-h-[116px] max-w-[345px] lg:max-w-[936px] md:max-w-[750px] flex flex-col text-center space-y-7 mt-20 m-2">
